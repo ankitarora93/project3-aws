3dd4a9e1741aa7d602561c731425420abf879b96
Ankit Arora <ankit.arora@olx.com>
2021-11-17 02:33:11 +0530

    initial commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgresql
+export POSTGRES_PASSWORD=postgresql
+export POSTGRES_HOST=cdr.cuqsiqxavcbb.ap-south-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=test-498685819112-dev2
+export AWS_REGION=ap-south-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
